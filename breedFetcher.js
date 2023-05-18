9f7c93e85783e64f398e4711f94f1b5906ab4876
Ryan Stewart <ryanstew17@gmail.com>
2023-05-18 12:26:29 -0300

    refactoring code and testing with chai and mocha

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,28 @@
 const request = require('request');
+const { breedName } = require('./index');
 
-const breedName = process.argv[2];
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(url, (error, resp, body) => {
-  // console.log(body);
-  // console.log(typeof body);
 
-  if (error) {
-    return console.log('Failed to request details: ', error);
-  }
+const fetchBreedDescription = function(breedName, callback) {
 
-  const data = JSON.parse(body);
-  // console.log(data);
+  request(url, (error, resp, body) => {
+  
+    if (error) {
+      return callback('Failed to request details: ', error);
+    }
+  
+    const data = JSON.parse(body);
+  
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback(`Failed to find breed ${breedName}`, null);
+    }
+  });
 
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`Failed to find breed ${breedName}`);
-  }
-});
+};
+
+
+module.exports = { fetchBreedDescription };
